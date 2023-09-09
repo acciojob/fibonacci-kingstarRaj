90995f4652a9a1088c307acbc0be9d25690b7afd
RAJA SAHU <117908592+kingstarRaj@users.noreply.github.com>
2023-09-09 10:46:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function fibonacci(num) {
-// your code here
+	if(num==0) return 0;
+	if(num==1) return 1;
+	let first  = 0 ;
+	let secno  = 1 ;
+	let third; 
+	while(num>1){
+		third = first + secno;
+		first = secno;
+		secno = third;
+		num--;
+	}
+	return secno;
 }
 
 module.exports = fibonacci;
