9e68805d02926718a4228ddf50260d6e94617faf
RAJA SAHU <117908592+kingstarRaj@users.noreply.github.com>
2023-09-11 22:39:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 
 function fibonacci(num) {
-	if(num==0) return 0;
-	if(num==1) return 1;
+	
+	if(num==1) return 0;
+	
 	let first  = 0 ;
 	let secno  = 1 ;
 	let third; 
-	while(num>1){
+	while(num > 2){
 		third = first + secno;
 		first = secno;
 		secno = third;
